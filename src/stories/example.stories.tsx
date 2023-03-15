55e7b5d2a809613f119acefdb64743a6a897627d
Gabriel GT <gabrielavila04@protonmail.com>
2023-03-14 21:36:41 -0300

    feat: :boom: finishes basic triggerState

--- src/stories/example.stories.tsx
+++ src/stories/example.stories.tsx
@@ -1,12 +1,50 @@
 /* eslint-disable @typescript-eslint/consistent-type-assertions */
-import React from "react";
+import React, { useEffect } from "react";
+import { useTriggerState } from "../trigger";
 
 export default {
   title: "Example",
 };
 
 const Template: React.FC = () => {
-  return <div>FUNCIONANDO!</div>;
+  const [state, setState] = useTriggerState({ name: "example", initial: "KKKKKKKKKKKKK" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+
+  return (
+    <center>
+      <br />
+      <br />
+      <br />
+      <input value={state} onChange={handleChange} />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Child />
+    </center>
+  );
+};
+
+const Child: React.FC = () => {
+  const [state] = useTriggerState({ name: "example", initial: true });
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  return (
+    <div>
+      This is the value in the freaking child!!! look at it!!!
+      <br />
+      here:
+      <br />
+      {state}
+    </div>
+  );
 };
 
 export const Example = Template.bind({});
--- src/trigger/useTriggerState.ts
+++ src/trigger/useTriggerState.ts
@@ -1,5 +1,51 @@
-function useTriggerState() {
-  return { a: "aaa" };
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const sessionState = (initial: any, name: string) => {
+  const value = sessionStorage.getItem(name);
+
+  if (value != null) {
+    return JSON.parse(value);
+  }
+
+  return initial;
+};
+
+function useTriggerState({ name, initial }: { name: string; initial: any }) {
+  const event = useRef<CustomEvent<{ [key in string]: any }> | null>(null);
+
+  const [state, setState] = useState(sessionState(initial, name));
+
+  useEffect(() => {
+    const listener = (ev: any) => {
+      const value = ev.detail[name];
+      setState(value);
+    };
+
+    // add event listener for the event
+    document.addEventListener(name, listener);
+
+    return () => {
+      // remove event listener for the event
+      document.removeEventListener(name, listener);
+    };
+  }, [name]);
+
+  const trigger = useCallback(
+    (value: any) => {
+      event.current = new CustomEvent(name, {
+        detail: {
+          [name]: value,
+        },
+      });
+
+      sessionStorage.setItem(name, JSON.stringify(value));
+
+      document.dispatchEvent(event.current);
+    },
+    [name]
+  );
+
+  return [state, trigger];
 }
 
 export default useTriggerState;
